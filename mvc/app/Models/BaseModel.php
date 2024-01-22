f6faddb26e531cc9cca3218aa3f835115f74a109
ngocbq3 <ngocbq@fpt.edu.vn>
2024-01-22 10:00:08 +0700

    update basemodel

--- mvc/app/Models/BaseModel.php
+++ mvc/app/Models/BaseModel.php
@@ -18,7 +18,8 @@ class BaseModel
     protected $conn;
     protected $sqlBuilder;
     protected $tableName;
-
+    //Đặt tên cột khóa chính
+    protected $primaryKey = "id";
     public function __construct()
     {
         $host = HOSTNAME;
@@ -41,5 +42,21 @@ class BaseModel
         $stmt = $model->conn->prepare($model->sqlBuilder);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_CLASS);
+    }
+    //Method find: tìm kiếm dữ liệu theo id
+    //@id: tham số truyền vào
+    public static function find($id)
+    {
+        $model = new static;
+        $model->sqlBuilder = "SELECT * FROM $model->tableName WHERE $model->primaryKey=:$model->primaryKey";
+        $stmt = $model->conn->prepare($model->sqlBuilder);
+        $data = ["$model->primaryKey" => $id];
+        $stmt->execute($data);
+        $result = $stmt->fetchAll(PDO::FETCH_CLASS);
+        //Kiểm tra xem có lấy được dữ liệu không
+        if ($result) {
+            return $result[0]; //lấy phần tử đầu tiên
+        }
+        return $result;
     }
 }
